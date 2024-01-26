181b9e7f90dc1752152d4b53d05b42582f0e0050
Yoga1654 <kit.25.21bcb060@gmail.com>
2024-01-26 13:19:23 +0000

    Calculator first commit

---
+++ Calculator.py
@@ -0,0 +1,62 @@
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+    
+    
+    
+# Function to multiply two numbers
+
+def multiply(x, y):
+
+    return x * y
+
+
+# Function to divide two numbers
+
+def divide(x, y):
+
+    if y == 0:
+
+        return "Cannot divide by zero"
+
+    return x / y
+
+# Main program loop
+while True:
+    # Display menu
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'quit' to end the program")
+
+    # User input
+    user_input = input(": ")
+
+    # Check if the user wants to quit
+    if user_input == "quit":
+        break
+
+    # Check for valid operation
+    if user_input in ("add", "subtract", "multiply", "divide"):
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+
+        if user_input == "add":
+            print("Result:", add(num1, num2))
+        elif user_input == "subtract":
+            print("Result:", subtract(num1, num2))
+        elif user_input == "multiply":
+
+	        print("Result:", multiply(num1, num2))
+
+        elif user_input == "divide":
+
+	        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
